9024be4fbe00a653cd428e592f0c075d6cd04947
Ninh <buithininhninh96@gmail.com>
2023-06-01 17:11:37 +0800

    edited readme.md files, edited testing.js, tested testing.js, resulting in creation of Copy text file.

--- testing.js
+++ testing.js
@@ -5,26 +5,45 @@ const path = require("path")
 
 //DEFINING VARS______________________________________
 //defining variables on file and folder names:
-Folder_res= "res files"
-TxtFile_org = "originalTextFile.txt"
+const Folder_res= "res files"
+const file_lorem = "Lorem.txt"
+const file_copy = "Copy"
 
-const textFile = path.join(__dirname, Folder_res, TxtFile_org)
+const textFile = path.join(__dirname, Folder_res, file_lorem)
+const newTxtFile = path.join(__dirname, Folder_res, file_copy)
 
+const newFileMessage = "Following text originates from " + file_lorem + ":\n"
 
 //FUNCTIONS_________________________________________
-//defining callback function
-function callback (error, contentOfFile) {
+function ifError(err) {
+    if (err) throw err
+}
 
-    console.log("Reading file now...")
 
-    if (error) throw error
-    fileContent = contentOfFile.toString()
+//defining error callback function
+function errorhandling (error, contentOfFile) {
 
-    console.log("File has been read.")
-
+    console.log("Reading file now...")//solely for testing 
     
+    ifError(error)
+    fileContent = contentOfFile.toString()
+    
+    console.log("File has been read.")//solely for testing 
+    
+    toInput = newFileMessage + fileContent
+
+    //attempting to create a new file and add content in contentOfFile
+    fs.appendFile(newTxtFile, toInput, (err)=>{ 
+        
+        console.log("Creating file now...")//solely for testing 
+        ifError(err)
+        console.log("File has been created.")//solely for testing 
+
+        
+
+    })
 
 }
 
 //EXECUTION___________________________________________
-fs.readFile(textFile, 'utf-8', callback)
+fs.readFile(textFile, 'utf-8', errorhandling)
